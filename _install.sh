c0949dfbf69df4b893aefad18fefe80c0e2759c2
nealriley <neal.riley@gmail.com>
2024-01-19 22:08:24 +0000

    tauri init

---
+++ _install.sh
@@ -0,0 +1,6 @@
+
+sudo apt install libwebkit2gtk-4.0-dev     build-essential     curl     wget     file     libssl-dev     libgtk-3-dev     libayatana-appindicator3-dev     librsvg2-dev
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+cd tauri-next
+npm install
+npm run tauri dev
