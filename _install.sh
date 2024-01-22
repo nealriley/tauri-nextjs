381ab84306318af4afae53c244f73fe7d75ddde9
Neal Riley <neal.riley@gmail.com>
2024-01-22 09:58:42 +0000

    added all of the three js for fun and profit

--- _install.sh
+++ _install.sh
@@ -1,6 +1,8 @@
 
-sudo apt install libwebkit2gtk-4.0-dev     build-essential     curl     wget     file     libssl-dev     libgtk-3-dev     libayatana-appindicator3-dev     librsvg2-dev
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+sudo apt update
+sudo apt install -y libjavascriptcoregtk-4.0-dev libsoup2.4-dev libwebkit2gtk-4.0-dev     build-essential     curl     wget     file     libssl-dev     libgtk-3-dev     libayatana-appindicator3-dev     librsvg2-dev 
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
+source "/workspace/.cargo/env"
 cd tauri-next
 npm install
 npm run tauri dev
--- tauri-next/src-tauri/src/main.rs
+++ tauri-next/src-tauri/src/main.rs
@@ -1,15 +1,37 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu, Builder, WindowBuilder};
+use std::process::Command;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let mut echo_hello = Command::new("sh");
+    echo_hello.arg("-c").arg(name);
+    let hello_1 = echo_hello.output().expect("failed to execute process");
+    let stdout = String::from_utf8_lossy(&hello_1.stdout);
+    println!("{}", stdout);
+    format!("{}", stdout)   
 }
 
 fn main() {
-    tauri::Builder::default()
+    let fullscreen = CustomMenuItem::new("fullscreen".to_string(), "Enter Fullscreen");
+    let standard = CustomMenuItem::new("standard".to_string(), "Leave Fullscreen");
+    let menu = Menu::new().add_item(fullscreen).add_item(standard);
+    
+    Builder::default()
+        .menu(menu)
         .invoke_handler(tauri::generate_handler![greet])
+        .on_menu_event(|event| {
+            if event.menu_item_id() == "fullscreen" {
+                // Toggle fullscreen here
+                event.window().set_fullscreen(true);
+            } else if (event.menu_item_id() == "standard") {
+                event.window().set_fullscreen(false);
+            }
+        })
+        
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+
